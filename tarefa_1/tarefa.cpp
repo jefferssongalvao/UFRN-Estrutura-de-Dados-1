2f670c948b4592cc2c1b421334643472a0de195b
Jeffersson Galvão <jeffersson.webdeveloper@gmail.com>
2014-02-26 14:56:20 -0300

    Correção de erro na no algoritmo ordenação selectionSort e criação de vetores especificos para cada algoritmo de ordenação

--- tarefa_1/tarefa.cpp
+++ tarefa_1/tarefa.cpp
@@ -41,36 +41,42 @@
      * Array do segundo cenário.
      * Vetor em ordem crescente com N elementos com valores entre min e max
      */
-        int geraArrayAleatorio_crescente(int v[], int N, int min) {
+        int geraArrayAleatorio_crescente(int v[], int N) {
             srand(time(NULL));  
             // alocação de variável
                 int *i = new int;
+                int *min = new int;
+                *min = 0;
 
             for (*i = 0; *i < N; ++(*i)) {
-                v[*i] = (rand() % 100) + min;
-                min = v[*i];
+                v[*i] = (rand() % 100) + *min;
+                *min = v[*i];
             }
 
             // desalocação de variável
                 delete i;
+                delete min;
         }
 
     /* 
      * Array do terceiro cenário.
      * Vetor em ordem decrescente com N elementos com valores entre min e max
      */
-        int geraArrayAleatorio_decrescente(int v[], int N, int min) {
+        int geraArrayAleatorio_decrescente(int v[], int N) {
             srand(time(NULL));  
             // alocação de variável
                 int *i = new int;
+                int *min = new int;
+                *min = 0;
 
             for (*i = N-1; *i >= 0; --(*i)) {
-                v[*i] = (rand() % 100) + min;
-                min = v[*i];
+                v[*i] = (rand() % 100) + *min;
+                *min = v[*i];
             }
 
             // desalocação de variável
                 delete i;
+                delete min;
         }
 
 /*
@@ -86,7 +92,7 @@
 
             for (*i = 0; *i < N-1; (*i)++) {
                 // acha o mínimo
-                    min = i;
+                    *min = *i;
                     for (*j = *i+1; *j < N; (*j)++)
                         if (v[*j] < v[*min])
                             *min = *j;
@@ -158,21 +164,29 @@
  */
     int testeAlgoritmo(int op, double *time1, double *time2, double *time3, int size){
         struct timeval t0, t1; // estruturas usadas para cálculo do tempo de execução
-        int *v = new int[size]; // alocação do vetor
+        int *v = new int[size]; // alocação do vetor selection
+        int *w = new int[size]; // alocação do vetor bubble
+        int *k = new int[size]; // alocação do vetor insertion
 
         // Escolha do cenário
             switch(op){
                 // Primeiro cenário
                     case 1:
-                        geraArrayAleatorio(v, size); // criação do array
+                        geraArrayAleatorio(v, size); // criação do array selection
+                        geraArrayAleatorio(w, size); // criação do array bubble
+                        geraArrayAleatorio(k, size); // criação do array insertion
                         break;
                 // Segundo cenário
                     case 2:
-                        geraArrayAleatorio_crescente(v, size, 0);  // criação do array
+                        geraArrayAleatorio_crescente(v, size);  // criação do array selection
+                        geraArrayAleatorio_crescente(w, size);  // criação do array bubble
+                        geraArrayAleatorio_crescente(k, size);  // criação do array insertion
                         break;
                 // terceiro cenário
                     case 3:
-                        geraArrayAleatorio_decrescente(v, size, 0);  // criação do array
+                        geraArrayAleatorio_decrescente(v, size);  // criação do array selection
+                        geraArrayAleatorio_decrescente(w, size);  // criação do array bubble
+                        geraArrayAleatorio_decrescente(k, size);  // criação do array insertion
                         break;
             }
         // Execução dos algoritmos de ordenação
@@ -186,14 +200,14 @@
             // bubbleSort
                 gettimeofday(&t0, NULL);
                     *time2  = 0.0;
-                    bubbleSort(v, size);
+                    bubbleSort(w, size);
                 gettimeofday(&t1, NULL);
                 (*time2) += ((t1.tv_sec-t0.tv_sec)*1000000 + t1.tv_usec-t0.tv_usec)/(double)1000;
 
             // insertionSort
                 gettimeofday(&t0, NULL);
                     *time3  = 0.0;
-                    insertionSort(v, size);
+                    insertionSort(k, size);
                 gettimeofday(&t1, NULL);
                 (*time3) += ((t1.tv_sec-t0.tv_sec)*1000000 + t1.tv_usec-t0.tv_usec)/(double)1000;
 
@@ -222,10 +236,10 @@ int main() {
         Escrita << "Cenário " << *i << ".\n";
         for(*j = 0; *j < 5; (*j)++){
             testeAlgoritmo(*i, time1, time2, time3, tamanhos[*j]);
-            Escrita << "Tamanho do vetor:" << tamanhos[*j] << ".\n";
-            Escrita << "Tempo decorrido selectionSort: " << *time1 << ".\n";
-            Escrita << "Tempo decorrido bubbleSort: " << *time2 << ".\n";
-            Escrita << "Tempo decorrido insertionSort: " << *time3 << ".\n\n";
+            Escrita << "Tamanho do vetor:" << tamanhos[*j] << "\n";
+            Escrita << "Tempo decorrido selectionSort: " << *time1 << "\n";
+            Escrita << "Tempo decorrido bubbleSort: " << *time2 << "\n";
+            Escrita << "Tempo decorrido insertionSort: " << *time3 << "\n\n";
         }
     }
 
