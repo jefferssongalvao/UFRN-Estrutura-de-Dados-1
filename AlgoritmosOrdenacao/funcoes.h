6b00c20f599491d9e3c1b64a328bc9b4cdc84968
Jeffersson Galvão <jeffersson.webdeveloper@gmail.com>
2014-03-12 14:43:23 -0300

    Criação de uma biblioteca de funções utilizadas nos testes dos algoritmos de ordenação, fazendo a adição dessa biblioteca nos arquivos dos algoritmos selection, bubble e radix

---
+++ AlgoritmosOrdenacao/funcoes.h
@@ -0,0 +1,38 @@
+/* 
+ * Funções auxiliares
+ */
+	// função para gerar um vetor de forma aleatória
+		 void geraArrayAleatorio(int v[], int N) {
+		    srand(time(NULL));
+		    // alocação de variável
+		        int *i = new int;
+
+		    for(*i = 0; *i < N; (*i)++)
+		    	v[*i] = (rand() % 100);
+
+		    // desalocação de variável
+		        delete i;
+		}
+	// Função para imprimir vetores
+		void imprimir(int v[], int n){
+		    // alocação de variável
+		        int *i = new int;
+
+			for(*i = 0; *i < n; (*i)++)
+				std::cout << v[*i] << " ";
+			std::cout << "\n";
+
+		    // desalocação de variável
+		        delete i;
+		}
+	// Função preenche um vetor com 0 em todas as posições do vetor
+		void preenche(int v[], int n){
+			// alocação de variável
+		        int *i = new int;
+
+			for(*i = 0; *i < n; (*i)++)
+				v[*i] = 0;
+
+		    // desalocação de variável
+		        delete i;
+		}
--- AlgoritmosOrdenacao/selectionSort.cpp
+++ AlgoritmosOrdenacao/selectionSort.cpp
@@ -7,45 +7,8 @@
 
 #include <iostream>
 #include <cstdlib> // para uso da função hand()
+#include "funcoes.h" // biblioteca de funções auxiliares para essa aplicação
 
-/* 
- * Funções auxiliares
- */
-	// função para gerar um vetor de forma aleatória
-		 void geraArrayAleatorio(int v[], int N) {
-		    srand(time(NULL));
-		    // alocação de variável
-		        int *i = new int;
-
-		    for(*i = 0; *i < N; (*i)++)
-		    	v[*i] = (rand() % 100);
-
-		    // desalocação de variável
-		        delete i;
-		}
-	// Função para imprimir vetores
-		void imprimir(int v[], int n){
-		    // alocação de variável
-		        int *i = new int;
-
-			for(*i = 0; *i < n; (*i)++)
-				std::cout << v[*i] << " ";
-			std::cout << "\n";
-
-		    // desalocação de variável
-		        delete i;
-		}
-	// Função preenche um vetor com 0 em todas as posições do vetor
-		void preenche(int v[], int n){
-			// alocação de variável
-		        int *i = new int;
-
-			for(*i = 0; *i < n; (*i)++)
-				v[*i] = 0;
-
-		    // desalocação de variável
-		        delete i;
-		}
 // Selection Sort
 	void selectionSort(int v[], int n){
 		// alocação das váriaveis
